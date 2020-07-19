b9fe29e8ca1f8a68596ca92cbaffb9df13cd2ac5
sntg99 <68486877+sntg99@users.noreply.github.com>
2020-07-18 22:17:47 -0300

    Update Ciudad.java

--- Grafos/Parcial - 1F/Ciudad.java
+++ Grafos/Parcial - 1F/Ciudad.java
@@ -1,7 +1,23 @@
+package prog3.grafos.utiles.parcial;
+
 public class Ciudad {
+	private String nombre;
+	private int fase; 
+	public Ciudad () {}
+	public Ciudad (String nombre, int fase) {
+		this.nombre=nombre;
+		this.fase=fase;
+	}
+	public int getFase () {
+		return this.fase;
+	}
+	public void setFase (int fase) {
+		this.fase=fase;
+	}
 	public String getNombre () {
 		return this.nombre;
 	}
 	public void setNombre (String nombre) {
 		this.nombre=nombre;
 	}
+}
